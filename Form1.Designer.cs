e531045f39d97d7bea9fccf8dcd170c26284e192
Дмитрий <rabolan@mail.ru>
2019-11-28 17:42:06 +0300

    настроенно удаление по имени процесса

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -32,6 +32,8 @@
             this.tabPage1 = new System.Windows.Forms.TabPage();
             this.clientTable = new System.Windows.Forms.DataGridView();
             this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.label5 = new System.Windows.Forms.Label();
             this.button4 = new System.Windows.Forms.Button();
             this.checkBox1 = new System.Windows.Forms.CheckBox();
             this.label3 = new System.Windows.Forms.Label();
@@ -45,14 +47,12 @@
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.label4 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
-            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
             this.tabControl1.SuspendLayout();
             this.tabPage1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.clientTable)).BeginInit();
             this.tabPage2.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
             this.SuspendLayout();
             // 
             // tabControl1
@@ -71,10 +71,10 @@
             this.tabPage1.AutoScroll = true;
             this.tabPage1.BackColor = System.Drawing.Color.DarkKhaki;
             this.tabPage1.Controls.Add(this.clientTable);
-            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Location = new System.Drawing.Point(4, 25);
             this.tabPage1.Name = "tabPage1";
             this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(541, 233);
+            this.tabPage1.Size = new System.Drawing.Size(541, 230);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "Активные процессы";
             // 
@@ -88,7 +88,7 @@
             this.clientTable.ReadOnly = true;
             this.clientTable.RowHeadersWidth = 51;
             this.clientTable.RowTemplate.Height = 24;
-            this.clientTable.Size = new System.Drawing.Size(535, 227);
+            this.clientTable.Size = new System.Drawing.Size(535, 224);
             this.clientTable.TabIndex = 0;
             this.clientTable.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.clientTable_CellContentClick);
             this.clientTable.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.clientTable_CellDoubleClick);
@@ -106,12 +106,33 @@
             this.tabPage2.Controls.Add(this.label1);
             this.tabPage2.Controls.Add(this.numericUpDown1);
             this.tabPage2.Controls.Add(this.buttonSaveSettings);
-            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Location = new System.Drawing.Point(4, 25);
             this.tabPage2.Name = "tabPage2";
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(541, 233);
+            this.tabPage2.Size = new System.Drawing.Size(541, 230);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "Настройки закрытия процессов";
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(24, 75);
+            this.numericUpDown2.Maximum = new decimal(new int[] {
+            1000000,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(230, 22);
+            this.numericUpDown2.TabIndex = 8;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(18, 59);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(262, 17);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "ID процесса которое нужно закрывать";
             // 
             // button4
             // 
@@ -130,7 +151,7 @@
             this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
             this.checkBox1.Location = new System.Drawing.Point(24, 134);
             this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(15, 14);
+            this.checkBox1.Size = new System.Drawing.Size(18, 17);
             this.checkBox1.TabIndex = 6;
             this.checkBox1.UseVisualStyleBackColor = true;
             this.checkBox1.CheckedChanged += new System.EventHandler(this.CheckBox1_CheckedChanged);
@@ -140,7 +161,7 @@
             this.label3.AutoSize = true;
             this.label3.Location = new System.Drawing.Point(21, 109);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(183, 13);
+            this.label3.Size = new System.Drawing.Size(233, 17);
             this.label3.TabIndex = 5;
             this.label3.Text = "Автозагрузка при старте системы";
             // 
@@ -149,7 +170,7 @@
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(21, 7);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(216, 13);
+            this.label2.Size = new System.Drawing.Size(276, 17);
             this.label2.TabIndex = 4;
             this.label2.Text = "Имя процесса которое нужно закрывать";
             // 
@@ -157,7 +178,7 @@
             // 
             this.textBox1.Location = new System.Drawing.Point(21, 27);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(233, 20);
+            this.textBox1.Size = new System.Drawing.Size(233, 22);
             this.textBox1.TabIndex = 3;
             // 
             // label1
@@ -165,7 +186,7 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(18, 163);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(255, 13);
+            this.label1.Size = new System.Drawing.Size(325, 17);
             this.label1.TabIndex = 2;
             this.label1.Text = "Через сколько закрывать программу  (Минутах)";
             // 
@@ -183,7 +204,7 @@
             0,
             0});
             this.numericUpDown1.Name = "numericUpDown1";
-            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 22);
             this.numericUpDown1.TabIndex = 1;
             this.numericUpDown1.Value = new decimal(new int[] {
             5,
@@ -203,10 +224,10 @@
             // 
             // tabPage3
             // 
-            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Location = new System.Drawing.Point(4, 25);
             this.tabPage3.Name = "tabPage3";
             this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage3.Size = new System.Drawing.Size(541, 233);
+            this.tabPage3.Size = new System.Drawing.Size(541, 230);
             this.tabPage3.TabIndex = 2;
             this.tabPage3.Text = "Автозапуск нужных приложений";
             this.tabPage3.UseVisualStyleBackColor = true;
@@ -214,9 +235,9 @@
             // button1
             // 
             this.button1.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.button1.Location = new System.Drawing.Point(0, 337);
+            this.button1.Location = new System.Drawing.Point(0, 329);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(575, 64);
+            this.button1.Size = new System.Drawing.Size(573, 64);
             this.button1.TabIndex = 1;
             this.button1.Text = "Выход";
             this.button1.UseVisualStyleBackColor = true;
@@ -226,7 +247,7 @@
             // 
             this.button2.Location = new System.Drawing.Point(19, 12);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(168, 23);
+            this.button2.Size = new System.Drawing.Size(168, 28);
             this.button2.TabIndex = 2;
             this.button2.Text = "Получить список процессов";
             this.button2.UseVisualStyleBackColor = true;
@@ -247,35 +268,14 @@
             this.label4.AutoSize = true;
             this.label4.Location = new System.Drawing.Point(209, 21);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.Size = new System.Drawing.Size(46, 17);
             this.label4.TabIndex = 4;
             this.label4.Text = "label4";
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(18, 59);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(205, 13);
-            this.label5.TabIndex = 7;
-            this.label5.Text = "ID процесса которое нужно закрывать";
-            // 
-            // numericUpDown2
-            // 
-            this.numericUpDown2.Location = new System.Drawing.Point(24, 75);
-            this.numericUpDown2.Maximum = new decimal(new int[] {
-            1000000,
-            0,
-            0,
-            0});
-            this.numericUpDown2.Name = "numericUpDown2";
-            this.numericUpDown2.Size = new System.Drawing.Size(230, 20);
-            this.numericUpDown2.TabIndex = 8;
             // 
             // Form1
             // 
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
-            this.ClientSize = new System.Drawing.Size(575, 401);
+            this.ClientSize = new System.Drawing.Size(573, 393);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
@@ -292,8 +292,8 @@
             ((System.ComponentModel.ISupportInitialize)(this.clientTable)).EndInit();
             this.tabPage2.ResumeLayout(false);
             this.tabPage2.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
--- Form1.cs
+++ Form1.cs
@@ -133,11 +133,12 @@ namespace GetProcesses
 
         //    // bl.KillProssec(Convert.ToInt32(temp));
         //    //  e.RowIndex
-        
+
         //}
 
-        private void clientTable_CellDoubleClick(object sender, System.EventArgs e)
+        private void clientTable_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
         {
+            Bl bl = new Bl();
 
             #region Тест 1
             //Int32 selectedCellCount =
@@ -192,38 +193,79 @@ namespace GetProcesses
 
             #endregion
 
+            #region не раб
+            //    Int32 selectedCellCount =
+            //clientTable.GetCellCount(DataGridViewElementStates.Selected);
+            //    if (selectedCellCount > 0)
+            //    {
+            //        if (clientTable.AreAllCellsSelected(true))
+            //        {
+            //            MessageBox.Show("НЕт Выбраны строки", "111");
+            //        }
+            //        else
+            //        {
+            //            System.Text.StringBuilder sb =
+            //                new System.Text.StringBuilder();
 
-            Int32 selectedCellCount =
-        clientTable.GetCellCount(DataGridViewElementStates.Selected);
-            if (selectedCellCount > 0)
-            {
-                if (clientTable.AreAllCellsSelected(true))
-                {
-                    MessageBox.Show("НЕт Выбраны строки", "111");
-                }
-                else
-                {
-                    System.Text.StringBuilder sb =
-                        new System.Text.StringBuilder();
+            //            for (int i = 0;
+            //                i < selectedCellCount; i++)
+            //            {
+            //                sb.Append("Row: ");
+            //                sb.Append(clientTable.SelectedCells[i].RowIndex
+            //                    .ToString());
+            //                //sb.Append(", Column: ");
+            //                //sb.Append(dataGridView1.SelectedCells[i].ColumnIndex
+            //                //    .ToString());
+            //                //sb.Append(Environment.NewLine);
+            //            }
 
-                    for (int i = 0;
-                        i < selectedCellCount; i++)
-                    {
-                        sb.Append("Row: ");
-                        sb.Append(clientTable.SelectedCells[i].RowIndex
-                            .ToString());
-                        //sb.Append(", Column: ");
-                        //sb.Append(dataGridView1.SelectedCells[i].ColumnIndex
-                        //    .ToString());
-                        //sb.Append(Environment.NewLine);
-                    }
+            //            sb.Append("Total: " + selectedCellCount.ToString());
+            //           // MessageBox.Show(sb.ToString(), "Selected Cells");
+            //        } 
+            //    }
 
-                    sb.Append("Total: " + selectedCellCount.ToString());
-                    MessageBox.Show(sb.ToString(), "Selected Cells");
-                }
+            #endregion
 
+            int temp = e.RowIndex;
+            string ggggg ="Незаполнено";
+              Int32 selectedRowCount =
+               clientTable.Rows.GetRowCount(DataGridViewElementStates.Selected);
 
-            }
+            //label4.Text = temp.ToString();
+
+            var ffff = clientTable.Rows.SharedRow(temp);
+
+            int counList = clientTable.RowCount; // получаем все значения 
+                                                 // var t = clientTable.Rows[temp]; // получили индек в датагрид
+                                                 // var ttt = t.Index; // интовы индекс
+
+            // for (int i = 0; i < counList; i++)
+            //{
+            //ggggg = clientTable.Rows[temp].Cells[temp].Value.ToString(); // работает 1раз
+            // ggggg = clientTable.CurrentRow.Cells.ToString(); // CurrentCell.ToString();
+            // this.clientTable.FirstDisplayedCell = this.clientTable.CurrentCell;
+            // ggggg = clientTable.FirstDisplayedCell.ToString();
+            //}
+
+            ggggg = clientTable.Rows[temp].Cells["ProcessName"].Value.ToString();
+
+            //DataGridViewRow startingBalanceRow = clientTable.Rows[temp];
+            //if (clientTable.SelectedRows.Contains(startingBalanceRow))
+            //{
+            //    // Do not allow the user to delete the Starting Balance row.
+            //    MessageBox.Show("FJFJFJFJFJFJFJ");
+
+            //    // Cancel the deletion if the Starting Balance row is included.
+            //   // e.Cancel = true;
+            //}
+
+            // if (tempListGetProcess.F == 1)
+            // обрабатывай данные строки
+
+            label4.Text = ggggg.ToString();
+
+            bl.KillProssec(ggggg); // Закрытие процесса по имени
+        }
 
 
 
